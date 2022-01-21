9f096c07c8085dbeb10a3c0a90bcd083a0095c6b
DMNight6 <50548798+DMNight6@users.noreply.github.com>
2022-01-21 22:03:03 +0800

    Remove audio player (too unstable)

--- src/component/CardRender/CardRender.jsx
+++ src/component/CardRender/CardRender.jsx
@@ -1,20 +1,12 @@
 import React, { useState } from 'react';
 import { Typography, CardActions, IconButton, Card, CardContent, CardMedia, NativeSelect, Box } from '@mui/material';
 import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import PauseIcon from '@mui/icons-material/Pause';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 
 const CardRender = ({product, download}) => { // I dunno what name I want to give so I'll call it product although it's data from backend
     const [Format, setFormat] = useState('.mp3');
-    const [AudioState, setAudioState] = useState(false)
 
     const handleButton = async () => await download(product.videoURL, product.name, Format);
 
-    const [audio] = useState(new Audio(`http://localhost:3001/audioRender?link=${product.videoURL}`));
-    const handleAudioButton = async() => {
-        AudioState ? audio.pause() : audio.play();
-        AudioState ? setAudioState(false) : setAudioState(true)
-    }
     return (
         <Card sx={{ display: 'flex'}}>
             <Box sx={{ display: 'flex', flexDirection: 'column'}}>
@@ -27,9 +19,6 @@ const CardRender = ({product, download}) => { // I dunno what name I want to giv
                     </Typography>
                 </CardContent>
                 <Box sx={{ display: 'flex', alignItems: 'center', pl: 1, pb: 1}}>
-                    <IconButton onClick={handleAudioButton}>
-                        {AudioState ? <PauseIcon /> : <PlayArrowIcon />}
-                    </IconButton>
                     <CardActions>
                         <NativeSelect id='format' onChange={e => setFormat(e.target.value)}>
                             <option value='.mp3'>MP3</option>
